1f9c7604b5906ba24b4d15cf5efe06c707bd391a
Jonaath <Jonaath@themison.com>
2026-04-07 17:27:14 +0530

    Security: permanently remove all log, env and secret files from git tracking

--- export-db.sh
+++
@@ -1,9 +0,0 @@
-﻿# Export database
-docker exec postgres-db pg_dump -U postgres -d postgres -F c -f /tmp/database_backup.dump
-
-# Copy to host
-docker cp postgres-db:/tmp/database_backup.dump /tmp/database_backup.dump
-
-# Also export as SQL for verification
-docker exec postgres-db pg_dump -U postgres -d postgres -f /tmp/database_backup.sql
-docker cp postgres-db:/tmp/database_backup.sql /tmp/database_backup.sql
