0c68c343ccaca282da707e9990e53b9d695327a8
sylwesterm <madartsoft@gmail.com>
2026-04-08 11:07:19 +0100

    Changed port to 8080

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@
 # pip install -r requirements.txt
 
 # Set port (Vercel will override this)
-PORT=${PORT:-8000}
+PORT=${PORT:-8080}
 
 # Start the FastAPI application with auto-reload
 exec uvicorn app.main:app --host 0.0.0.0 --port $PORT --reload
